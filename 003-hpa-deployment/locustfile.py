225695b024626218eb8e953e7fe231d9b8fcdc7e
Agfian (Boy) <muhammadagfian96@gmail.com>
2025-04-28 14:20:23 +0700

    Refactor code structure for improved readability and maintainability

---
+++ 003-hpa-deployment/locustfile.py
@@ -0,0 +1,18 @@
+# locustfile.py
+from locust import HttpUser, between, task
+
+
+class LoadTest(HttpUser):
+    wait_time = between(1, 3)
+
+    @task(1)
+    def test_root(self):
+        self.client.get("/system/hostname")
+
+    @task(2)
+    def test_cpu(self):
+        self.client.get("/performance/cpu")
+
+    @task(2)
+    def test_memory(self):
+        self.client.get("/performance/memory")
---
+++ 003-hpa-deployment/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 003-hpa-deployment!")
+
+
+if __name__ == "__main__":
+    main()
